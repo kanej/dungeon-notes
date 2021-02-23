9324ba1e4f4a4e512b223f0fba83442b5dbe5869
John Kane <john@kanej.me>
2021-02-23 20:41:40 +0000

    feat(editor): add heading/blockquote options
    Signed-off-by: John Kane <john@kanej.me>

--- packages/web/src/components/PluginEditor.tsx
+++ packages/web/src/components/PluginEditor.tsx
@@ -1,11 +1,16 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { createEditor } from 'slate'
 import { withHistory } from 'slate-history'
 import { Slate, withReact } from 'slate-react'
+import { useDebounce } from 'use-debounce'
+import { serialize } from 'remark-slate'
 import {
   FormatBold,
   FormatItalic,
+  FormatQuote,
   FormatUnderlined,
+  LooksOne,
+  ShortText,
 } from '@styled-icons/material'
 import {
   withMarks,
@@ -21,31 +26,82 @@ import {
   MARK_UNDERLINE,
   ItalicPlugin,
   UnderlinePlugin,
+  HeadingPlugin,
+  ELEMENT_H1,
+  ToolbarElement,
+  DEFAULTS_PARAGRAPH,
+  DEFAULTS_BLOCKQUOTE,
+  BlockquotePlugin,
 } from '@udecode/slate-plugins'
 
 const plugins = [
   BoldPlugin(),
   ItalicPlugin(),
   UnderlinePlugin(),
+  HeadingPlugin(),
+  BlockquotePlugin(),
   BasicMarkPlugins(),
   BasicElementPlugins(),
 ]
 
 const withPlugins = [withReact, withHistory, withMarks()] as const
 
+function mapType(v: any) {
+  if (v.type === 'h1') {
+    return { ...v, type: 'heading_one' }
+  }
+
+  if (v.type === 'h2') {
+    return { ...v, type: 'heading_two' }
+  }
+
+  if (v.type === 'blockquote') {
+    return { ...v, type: 'block_quote' }
+  }
+
+  return v
+}
+
+function unmapType(v: any) {
+  if (v.type === 'heading_one') {
+    return { ...v, type: 'h1' }
+  }
+
+  if (v.type === 'heading_two') {
+    return { ...v, type: 'h2' }
+  }
+
+  if (v.type === 'block_quote') {
+    return { ...v, type: 'blockquote' }
+  }
+
+  return v
+}
+
 export const Editor: React.FC<{
-  value: string
+  value: Array<any>
   onSave: (text: string) => void
 }> = ({ value: initialValue, onSave }) => {
   const editor = useMemo(() => pipe(createEditor(), ...withPlugins), [])
-  const [value, setValue] = useState<any>(
-    initialValue || [
-      {
-        type: 'paragraph',
-        children: [{ text: 'Start your adventure! NO really ...' }],
-      },
-    ],
+
+  const [value, setValue] = useState<any>(() =>
+    initialValue
+      ? initialValue.map(unmapType)
+      : [
+          {
+            type: 'paragraph',
+            children: [{ text: 'Start your adventure! NO really ...' }],
+          },
+        ],
   )
+
+  const [snapshot] = useDebounce(value, 3000)
+
+  useEffect(() => {
+    const markdown = snapshot.map((v: any) => serialize(mapType(v))).join('')
+
+    onSave(markdown)
+  }, [onSave, snapshot])
 
   return (
     <Slate
@@ -71,6 +127,24 @@ export const Editor: React.FC<{
           type={MARK_UNDERLINE}
           icon={<FormatUnderlined />}
           tooltip={{ content: 'Underline (⌘U)' }}
+        />
+        <ToolbarElement
+          reversed
+          type={DEFAULTS_PARAGRAPH.p.type!}
+          icon={<ShortText />}
+          tooltip={{ content: 'Paragraph' }}
+        />
+        <ToolbarElement
+          reversed
+          type={DEFAULTS_BLOCKQUOTE.blockquote.type!}
+          icon={<FormatQuote />}
+          tooltip={{ content: 'Blockquote' }}
+        />
+        <ToolbarElement
+          reversed
+          type={ELEMENT_H1}
+          icon={<LooksOne />}
+          tooltip={{ content: 'Heading' }}
         />
       </BalloonToolbar>
       <EditablePlugins plugins={plugins} placeholder="Enter some text..." />
