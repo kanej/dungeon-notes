a51f6e9139f22bb026d7749f38a76de37f4e4037
John Kane <john@kanej.me>
2021-10-22 17:28:54 +0100

    build: resolve build errors due to gatsby addition
    Signed-off-by: John Kane <john@kanej.me>

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -1,64 +1,65 @@
 module.exports = {
   siteMetadata: {
-    title: `Dungeon Notes`,
+    title: 'Dungeon Notes',
     author: {
-      name: `John Kane`,
-      summary: `a programmer reporting from North of the Wall`,
+      name: 'John Kane',
+      summary: 'a programmer reporting from North of the Wall',
     },
-    description: `Tools to aid the the Dungeon Master in their depraved adventures.`,
-    siteUrl: `https://dungeonnotes.com/`,
+    description:
+      'Tools to aid the the Dungeon Master in their depraved adventures.',
+    siteUrl: 'https://dungeonnotes.com/',
     social: {
-      twitter: `dungeonnotes`,
+      twitter: 'dungeonnotes',
     },
   },
   plugins: [
-    `gatsby-plugin-image`,
+    'gatsby-plugin-image',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`,
+        name: 'blog',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 630,
             },
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+              wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     // {
-    //   resolve: `gatsby-plugin-google-analytics`,
+    //   resolve: 'gatsby-plugin-google-analytics',
     //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
+    //     trackingId: 'ADD YOUR TRACKING ID HERE',
     //   },
     // },
     {
-      resolve: `gatsby-plugin-feed`,
+      resolve: 'gatsby-plugin-feed',
       options: {
         query: `
           {
@@ -110,21 +111,21 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Dungeon Notes`,
-        short_name: `dnotes`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/icon_512x512.svg`, // This path is relative to the root of the site.
+        name: 'Dungeon Notes',
+        short_name: 'dnotes',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/icon_512x512.svg', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // 'gatsby-plugin-offline',
     'gatsby-plugin-styled-components',
   ],
 }
